f270d3e37a36053dfd94cf3777be974b45c537f9
safi50 <63853856+safi50@users.noreply.github.com>
2022-04-09 01:25:05 +0500

    Add files via upload

---
+++ Bitcoin Price Ticker App/lib/coin_data.dart
@@ -0,0 +1,54 @@
+//2. Import the required packages.
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const bitcoinAverageURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apikey = 'A4CAA5FA-DDBF-4B93-91AF-BFAF1A81EC71';
+
+class CoinData {
+  //3: Update getCoinData to take the selectedCurrency as an input.
+  Future getCoinData(String selectedCurrency) async {
+    //4: Update the URL to use the selectedCurrency input.
+    String requestURL =
+        '$bitcoinAverageURL/BTC/$selectedCurrency?apikey=$apikey';
+    http.Response response = await http.get(Uri.parse(requestURL));
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      double lastPrice = decodedData['rate'];
+      return lastPrice.toStringAsFixed(0);
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
